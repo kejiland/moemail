0c0e924fd6fe02a1dbfa001ca0a203c2b280361b
kejiland <kejiland@azhz.eu.org>
2025-06-07 11:37:47 +0800

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 
 export const zpix = localFont({
-  src: '../public/fonts/zpix.ttf',
-  variable: '--font-zpix',
+  src: '../public/fonts/condensed_light.ttf',
+  variable: '--font-condense',
   display: 'swap',
 }) 
